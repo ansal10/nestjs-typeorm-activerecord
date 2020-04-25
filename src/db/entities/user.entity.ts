8ec96387b105376e2f6b1e412c9b43ccd940df03
Geek <geek@Geeks-MacBook-Pro.local>
2020-04-25 20:01:39 +0530

    Adding migrations and entity

--- src/db/entities/user.entity.ts
+++ src/db/entities/user.entity.ts
@@ -1,6 +1,15 @@
 import {Entity, PrimaryColumn, Column, BaseEntity} from "typeorm";
+import { TableName } from '../TableName';
 
-@Entity({name: 'user'})
+@Entity({name: TableName.User})
 export class UserEntity extends BaseEntity {
 
+  @PrimaryColumn()
+  id: number
+
+  @Column()
+  email: string;
+
+  @Column({name: 'full_name'})
+  fullName: string;
 }
--- src/db/migrations/1587784427638-create-user-table.ts
+++ src/db/migrations/1587784427638-create-user-table.ts
@@ -1,12 +1,39 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { TableName } from '../TableName';
 
 export class createUserTable1587784427638 implements MigrationInterface {
 
+    public userTable: Table = new Table({
+        name: TableName.User,
+        columns: [
+            {
+                name: 'id',
+                type: 'integer',
+                isGenerated: true,
+                isPrimary: true,
+                generationStrategy: 'increment'
+            },
+            {
+                name: 'email',
+                type: 'string',
+                isNullable: false,
+                isUnique: true,
+            },
+            {
+                name: 'full_name',
+                type: 'string',
+                isNullable: true,
+                isUnique: false
+            }
+        ]
+    })
 
     public up = async (queryRunner: QueryRunner) => {
+        await queryRunner.createTable(this.userTable);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable(this.userTable);
     }
 
 }
